e93a3d1728398344a697aa6bea1e48961160b7ae
Pooja Kumari <35136890+poojakumari11228@users.noreply.github.com>
2020-03-26 22:32:56 +0500

    Level03 Tasks

---
+++ 1.JavaSyntax/task03/task0302/Solution.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task03.task0302;
+
+/*
+Display right away
+
+*/
+public class Solution {
+    // write your code here
+
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+    public static void printString(String s){
+        System.out.print(s);
+    }
+}
